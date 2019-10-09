857f7af4a4ea319ebdfc62b616092e00bc191a7d
ysaadi <yaman.saadi@gmail.com>
2019-10-09 08:06:18 -0400

    fixed but no eof from netcat

--- main.go
+++ main.go
@@ -1,48 +1,47 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
 	"io"
 	"net"
 )
 
 func main() {
-	ln, err := net.Listen("tcp", ":8080")
-	if err != nil {
-		// handle error
-	}
 	ch := make(chan []byte, 1024)
+	defer close(ch)
+	go serve(ch)
 	for {
-		select {
-		case byteArray := <-ch:
-			fmt.Print(string(byteArray))
-		default:
-			conn, err := ln.Accept()
-			if err != nil {
-				fmt.Print(err)
-				// handle error
-			}
-
-			go handleConnection(conn, ch)
-		}
+		byteArray := <-ch
+		fmt.Print(string(byteArray))
 	}
 }
 
 func handleConnection(conn net.Conn, ch chan []byte) {
 	defer conn.Close()
-	defer close(ch)
-	result := bytes.NewBuffer(nil)
+	// result := bytes.NewBuffer(nil)
 	var buf [512]byte
 	for {
 		n, err := conn.Read(buf[0:])
-		result.Write(buf[0:n])
+		ch <- buf[0:n]
 		if err != nil {
 			if err == io.EOF {
 				break
 			}
-			ch <- []byte("why u error me")
+			ch <- []byte(err.Error())
 		}
 	}
-	ch <- result.Bytes()
+}
+func serve(ch chan []byte) {
+	ln, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		ch <- []byte(err.Error())
+	}
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			ch <- []byte(err.Error())
+		}
+
+		go handleConnection(conn, ch)
+	}
 }
